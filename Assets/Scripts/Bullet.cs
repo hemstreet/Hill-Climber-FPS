c10269fd1a39b4b3b00c44f0fb25500fa30bf5cd
Hemstreet <jonhemstreet@gmail.com>
2017-01-13 02:07:54 -0700

    updated gitignore, added Assets nad Lib

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject flame_prefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		string collisionTag = collision.gameObject.tag;
+
+		if (collisionTag == "Enemy") 
+		{
+			// For old dropping in flame animation and what not
+			// collision.gameObject.tag = "Untagged";
+			// Instantiate(flame_prefab, collision.transform.position, collision.transform.rotation);
+			Destroy(gameObject);
+
+			// @TODO Use take damage, to kill off enemy
+			// collision.gameObject.TakeDamage();	
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float health = 10f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void TakeDamage(int amount = 1) 
+	{
+		health -= amount;
+
+		if (health <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
