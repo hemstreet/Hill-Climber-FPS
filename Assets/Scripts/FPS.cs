352133081558c5c8028367df1a45290bcdd2e754
Hemstreet <jonhemstreet@gmail.com>
2017-01-12 21:30:57 -0700

    initial commit

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS : MonoBehaviour {
+
+	public GameObject bullet_prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			Fire ();
+		}
+	}
+
+	void Fire() {
+		Instantiate(bullet_prefab, this.transform.position, this.transform.rotation);
+	}
+}
