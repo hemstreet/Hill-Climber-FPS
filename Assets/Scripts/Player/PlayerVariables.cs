7c3ce4c7090de00049ba3f4375818b14f06f02ab
Hemstreet <jonhemstreet@gmail.com>
2017-01-21 13:33:56 -0700

    Code scaffolding for game mechanics

--- Assets/Scripts/Player/PlayerVariables.cs
+++ Assets/Scripts/Player/PlayerVariables.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
-public static class PlayerVariables
+namespace Player
 {
-    public static float health;
-    public static float ammunition;
-    public static Dictionary<string, float> armor;
+    public static class PlayerVariables
+    {
+        public static string username;
+        public static float health;
+        public static float ammunition;
+        public static Inventory inventory;
+        public static Dictionary<string, GameObject> armor;
+        public static Dictionary<string, GameObject> weapons;
 
+    }
 }
---
+++ Assets/Scripts/Score/Altitude.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Score
+{
+    public class Altitude : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+            // Get altitude rewards
+            // draw lines on map for their goals
+        }
+
+        // Update is called once per frame
+        void Update ()
+        {
+            float zAxis = gameObject.transform.position.z;
+        }
+    }
+}
